99fec579e276aad4959f6379c756d9bf228df5aa
sharvilak <sharvilak@e9ine.com>
2020-03-29 00:46:22 +0530

    test: brace-completion test added
    brace completion test added

---
+++ packages/@vuedx/typescript-plugin-vue/tests/e2e/project/fixture-brace-completions.vue
@@ -0,0 +1,19 @@
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  props: {
+    foo: String
+  },
+  setup(props) {
+    return {
+      bar: 'foo',
+      obj: {
+    }
+  }
+})
+</script>
+
+<template>
+  <div />
+</template>
