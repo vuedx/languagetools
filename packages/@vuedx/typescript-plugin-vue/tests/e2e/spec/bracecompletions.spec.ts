60d86250cc16be408caa6ea5547298d9b16834b7
Sharvilak <sharvilakt@gmail.com>
2020-04-01 06:28:19 +0530

    test(typescript-plugin-vue): LSP braceCompletion method

---
+++ packages/@vuedx/typescript-plugin-vue/tests/e2e/spec/bracecompletions.spec.ts
@@ -0,0 +1,21 @@
+import Proto from 'typescript/lib/protocol'
+import { createLanguageServerForTest } from '../server'
+
+describe('getBraceCompletions', () => {
+  test('should complete the opened braces', async () => {
+    const server = createLanguageServerForTest()
+    server.openFile('fixture-brace-completions.vue')
+    server.sendCommand(Proto.CommandTypes.BraceCompletion, <Proto.BraceCompletionRequestArgs>{
+      file: server.canonicalFileName('fixture-brace-completions.vue'),
+      line: 11,
+      offset: 12,
+      openingBrace: '{'
+    })
+
+    await server.close()
+    expect(server.responses).toHaveLength(1)
+    const response: Proto.BraceResponse = server.responses[0]
+    expect(response.command).toBe(Proto.CommandTypes.BraceCompletion)
+    expect(response.body).toBe(true)
+  })
+})
