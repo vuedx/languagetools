50f5cb741fa9192923760d02a656ee8c76bac98a
Rahul Kadyan <hi@znck.me>
2020-08-09 07:21:33 +0530

    fix: remove version check, .vue needs to be handled as vscode extension is already expection so

--- packages/typescript-plugin-vue/src/index.ts
+++ packages/typescript-plugin-vue/src/index.ts
@@ -1,5 +1,3 @@
-import FS from 'fs';
-import Path from 'path';
 import { PluginContext } from './context';
 import { Modules, TS } from './interfaces';
 import { RoutingLanguageServer } from './servers/routing';
@@ -7,7 +5,7 @@ import { RoutingLanguageServer } from './servers/routing';
 let context: PluginContext;
 let server: RoutingLanguageServer;
 
-export { PluginConfig, Modules } from './interfaces';
+export { Modules, PluginConfig } from './interfaces';
 
 export default function init({ typescript }: Modules): TS.server.PluginModule {
   if (!context) {
@@ -17,14 +15,8 @@ export default function init({ typescript }: Modules): TS.server.PluginModule {
 
   return {
     create(info) {
-      const version = findVueVersionIn(info.project.getCurrentDirectory());
-
-      if (!isVue3(version)) {
-        return info.languageService;
-      } else {
-        context.load(info);
-        return server.decorate(info.languageService);
-      }
+      context.load(info);
+      return server.decorate(info.languageService);
     },
 
     onConfigurationChanged(config) {
@@ -33,45 +25,4 @@ export default function init({ typescript }: Modules): TS.server.PluginModule {
       }
     },
   };
-}
-
-function isVue3(version: string | undefined) {
-  if (!version) return true; // assume Vue 3 by default.
-
-  return version.startsWith('3.');
-}
-
-function findVueVersionIn(directory: string): string | undefined {
-  const packageFile = findNearestPackageFile(directory);
-
-  if (packageFile) {
-    const vuePackageFile = Path.resolve(Path.dirname(packageFile), 'node_modules', 'vue', 'package.json');
-
-    if (FS.existsSync(vuePackageFile)) {
-      return require(vuePackageFile).version;
-    }
-
-    // dependencies not installed.
-    const pkg = require(packageFile);
-    const version = pkg.dependencies?.vue || pkg.devDependencies?.vue;
-    if (!version) return;
-
-    const justNumbers = version.replace(/[^0-9.]/, '');
-
-    if (justNumbers.startsWith('3.')) return justNumbers;
-    if (version === 'next') return '3.0.0';
-
-    return '2.x';
-  }
-}
-
-function findNearestPackageFile(directory: string): string | undefined {
-  if (!directory) return;
-
-  const packageFile = Path.resolve(directory, 'package.json');
-
-  if (FS.existsSync(packageFile)) return packageFile;
-  if (directory === '/') return;
-
-  return findNearestPackageFile(Path.dirname(directory));
 }
