c4c5a0342328e0cc0d6c19aa2626bab8e33f15dd
Rahul Kadyan <hi@znck.me>
2020-10-29 10:31:18 +0530

    fix: CI build script

--- scripts/build.js
+++ scripts/build.js
@@ -6,21 +6,25 @@ const bin = path.resolve(__dirname, '../node_modules/rollup/dist/bin/rollup')
 const env = '--environment BUILD:production'
 const args = '--max-old-space-size=1024'
 
-const standalone = ['typescript-plugin-vue']
+const standalone = []
+const ignored = ['typescript-standalone']
 
 names.forEach((name) => {
+  if (ignored.includes(name)) return
   console.log()
   console.log(`${name}: types`)
-  cp.execSync(`node ${args} ${bin} -c ${env},FILTER:${name},KIND:types`)
+  cp.execSync(`node ${args} ${bin} -c ${env},FILTER:${name},KIND:type`)
 })
 
 names.forEach((name) => {
+  if (ignored.includes(name)) return
   console.log()
   console.log(`${name}: main,module`)
-  cp.execSync(`node ${args} ${bin} -c ${env},FILTER:${name},KIND:default`)
+  cp.execSync(`node ${args} ${bin} -c ${env},FILTER:${name},KIND:bundle`)
 })
 
 standalone.forEach((name) => {
+  if (ignored.includes(name)) return
   console.log()
   console.log(`${name}: main,module`)
   cp.execSync(`node ${args} ${bin} -c ${env},FILTER:${name},KIND:standalone`)
