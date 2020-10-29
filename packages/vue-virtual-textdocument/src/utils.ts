fbd6fda8dc5f23513c428e9e3bf330ca5f7142f1
Rahul Kadyan <hi@znck.me>
2020-10-30 03:17:29 +0530

    chore: use basename from Node

--- packages/vue-virtual-textdocument/src/utils.ts
+++ packages/vue-virtual-textdocument/src/utils.ts
@@ -1,84 +1,90 @@
-import { SFCBlock } from '@vuedx/compiler-sfc';
-import { URI } from 'vscode-uri';
-import { Selector } from './types';
+import { SFCBlock } from '@vuedx/compiler-sfc'
+import { URI } from 'vscode-uri'
+import { Selector } from './types'
+import Path from 'path'
 
 export function isNotNull<T>(value: T | null | undefined): value is T {
-  return value != null;
+  return value != null
 }
 
 export function isString(value: unknown): value is string {
-  return typeof value === 'string';
+  return typeof value === 'string'
 }
 
 export function isNumber(value: unknown): value is number {
-  return typeof value === 'number';
+  return typeof value === 'number'
 }
 
 export function getLanguageIdFromExtension(ext: string) {
   switch (ext) {
     case 'js':
-      return 'javascript';
+      return 'javascript'
     case 'ts':
-      return 'typescript';
+      return 'typescript'
   }
 
-  return ext;
+  return ext
 }
 
 // Vue File: foo.vue
-export const VIRTUAL_FILENAME_SEPARATOR = '________';
+export const VIRTUAL_FILENAME_SEPARATOR = '________'
 
 export function basename(fileName: string) {
-  return fileName.split(/[/\\]/).pop()!;
+  return Path.basename(fileName)
 }
 
 export function relativeVirtualImportPath(fileName: string) {
-  return `./${basename(fileName).replace(/\.[\w]+$/, '')}`;
+  return `./${basename(fileName).replace(/\.[\w]+$/, '')}`
 }
 
 export function isVueFile(fileName: string) {
-  return fileName.endsWith('.vue');
+  return fileName.endsWith('.vue')
 }
 
 export function isVirtualFile(fileName: string) {
-  return fileName.indexOf('.vue' + VIRTUAL_FILENAME_SEPARATOR) > 0;
+  return fileName.indexOf('.vue' + VIRTUAL_FILENAME_SEPARATOR) > 0
 }
 
 export function getContainingFile(fileName: string) {
-  return fileName.split(VIRTUAL_FILENAME_SEPARATOR).shift()!;
+  return fileName.split(VIRTUAL_FILENAME_SEPARATOR).shift()!
 }
 
 export function asUri(fileName: string) {
-  if (/^[a-z-]+:\/\//i.test(fileName)) return fileName;
+  if (/^[a-z-]+:\/\//i.test(fileName)) return fileName
 
-  const uri = URI.file(fileName).toString();
+  const uri = URI.file(fileName).toString()
 
   if (isVirtualFile(fileName)) {
-    return uri.replace(/^[^:]+/, 'vue');
+    return uri.replace(/^[^:]+/, 'vue')
   }
 
-  return uri;
+  return uri
 }
 
 export function asFsPath(uri: string) {
-  return URI.parse(uri).fsPath;
+  return URI.parse(uri).fsPath
 }
 
 export function parseVirtualFileName(fileName: string) {
-  const uri = URI.parse(asUri(fileName));
+  const uri = URI.parse(asUri(fileName))
 
   if (uri.scheme === 'vue') {
-    const [container, selector] = uri.fsPath.split(VIRTUAL_FILENAME_SEPARATOR);
-    if (!selector.includes('.')) throw new Error('Malformated virtual file uri.');
-    const [block, index] = selector.substr(0, selector.lastIndexOf('.')).split('__');
+    const [container, selector] = uri.fsPath.split(VIRTUAL_FILENAME_SEPARATOR)
+    if (!selector.includes('.'))
+      throw new Error('Malformated virtual file uri.')
+    const [block, index] = selector
+      .substr(0, selector.lastIndexOf('.'))
+      .split('__')
 
     return {
       uri: URI.file(container).toString(),
-      selector: <Selector>(index ? { type: block, index: parseInt(index, 10) } : { type: block }),
-    };
+      selector: <Selector>(
+        (index ? { type: block, index: parseInt(index, 10) } : { type: block })
+      ),
+    }
   }
 
-  return null;
+  return null
 }
 
 const languages: Record<string, string> = {
@@ -88,24 +94,27 @@ const languages: Record<string, string> = {
   style: 'css',
   preview: 'vue-html',
   docs: 'markdown',
-};
-
-export function getBlockLanguage(block?: SFCBlock | null): string {
-  if (!block) return '';
-
-  if (block.lang) {
-    if (block.lang === 'js') return 'javascript';
-    if (block.lang === 'ts') return 'typescript';
-    return block.lang;
-  }
-
-  return languages[block.type] || block.type;
 }
 
-export function isOffsetInBlock(offset: number, block?: SFCBlock | null): boolean {
-  if (!block) return false;
+export function getBlockLanguage(block?: SFCBlock | null): string {
+  if (!block) return ''
 
-  return block.loc.start.offset <= offset && offset <= block.loc.end.offset;
+  if (block.lang) {
+    if (block.lang === 'js') return 'javascript'
+    if (block.lang === 'ts') return 'typescript'
+    return block.lang
+  }
+
+  return languages[block.type] || block.type
+}
+
+export function isOffsetInBlock(
+  offset: number,
+  block?: SFCBlock | null,
+): boolean {
+  if (!block) return false
+
+  return block.loc.start.offset <= offset && offset <= block.loc.end.offset
 }
 
 const extensions: Record<string, string> = {
@@ -114,23 +123,26 @@ const extensions: Record<string, string> = {
   typescript: 'ts',
   typescriptreact: 'tsx',
   markdown: 'md',
-};
+}
 export function getLanguageExtension(lang: string): string {
-  return extensions[lang] || lang;
+  return extensions[lang] || lang
 }
 
-export function binarySearch<T>(array: T[], isMatch: (a: T) => number): T | undefined {
-  let lo = 0;
-  let hi = array.length - 1;
+export function binarySearch<T>(
+  array: T[],
+  isMatch: (a: T) => number,
+): T | undefined {
+  let lo = 0
+  let hi = array.length - 1
 
   while (lo <= hi) {
-    const mid = ~~(lo + (hi - lo) / 2);
-    const result = isMatch(array[mid]);
+    const mid = ~~(lo + (hi - lo) / 2)
+    const result = isMatch(array[mid])
 
     if (result === 0) {
-      return array[mid];
+      return array[mid]
     }
-    if (result < 0) lo = mid + 1;
-    else hi = mid - 1;
+    if (result < 0) lo = mid + 1
+    else hi = mid - 1
   }
 }
