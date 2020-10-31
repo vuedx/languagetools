95ed6f1ce851465d10dc3960c985f7964d0afd1d
Rahul Kadyan <hi@znck.me>
2020-10-31 06:16:08 +0530

    test: diagnostics (#30)
    * test: setup fixtures
    
    * test: add baseline tests for diagnostics
    
    * feat: support goto definition on v-bind:prop

---
+++ samples/feature-diagnostics/src/MissingKeysInObjectProp.vue
@@ -0,0 +1,29 @@
+<script>
+import { defineComponent } from 'vue'
+import JavaScriptPropsOption from './components/JavaScriptPropsOption.vue'
+import JavaScriptSetupPropsOption from './components/JavaScriptSetupPropsOption.vue'
+import TypeScriptPropsOption from './components/TypeScriptPropsOption.vue'
+import TypeScriptPropsType from './components/TypeScriptPropsType.vue'
+import TypeScriptSetupPropsDeclare from './components/TypeScriptSetupPropsDeclare.vue'
+import TypeScriptSetupPropsOption from './components/TypeScriptSetupPropsOption.vue'
+
+export default defineComponent({
+  components: {
+    TypeScriptPropsType,
+    TypeScriptPropsOption,
+    TypeScriptSetupPropsDeclare,
+    TypeScriptSetupPropsOption,
+    JavaScriptPropsOption,
+    JavaScriptSetupPropsOption,
+  },
+})
+</script>
+
+<template>
+  <TypeScriptPropsType :object="{}" />
+  <TypeScriptPropsOption :object="{}" />
+  <TypeScriptSetupPropsDeclare :object="{}" />
+  <TypeScriptSetupPropsOption :object="{}" />
+  <JavaScriptPropsOption :object="{}" />
+  <JavaScriptSetupPropsOption :object="{}" />
+</template>
