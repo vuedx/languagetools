0e013a4f1a86300b455c3190255540f28eafd7b2
Rahul Kadyan <hi@znck.me>
2020-11-02 09:23:42 +0530

    chore: add eslint

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,5 +1,7 @@
 module.exports = {
-  '**/*.{js,jsx,json,ts,tsx,vue,yml,yaml}': ['prettier --write'],
+  'packages/*/src/**/*.ts': ['eslint --cache --fix', 'prettier --write'],
+  'extensions/*/src/**/*.ts': ['eslint --cache --fix', 'prettier --write'],
+  '**/*.{js,jsx,json,tsx,vue,yml,yaml}': ['prettier --write'],
   'samples/**/*.vue': [
     'jest -u ./packages/vue-virtual-textdocument/test/baseline.spec.ts',
     'git add **/*.snap',
