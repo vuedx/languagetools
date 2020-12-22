cda86a2f523f975603db8a2247fc8b29c371b31b
Rahul Kadyan <hi@znck.me>
2020-12-23 04:43:39 +0530

    ftest(#85): check prop types of plain JS components (#109)

---
+++ samples/feature-diagnostics/src/ComponentPropUsage.vue
@@ -0,0 +1,15 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld :msg="123" />
+</template>
+
+<script>
+import HelloWorldVue from "./components/HelloWorld.vue";
+import { defineComponent } from "vue";
+export default defineComponent({
+  name: "App",
+  components: {
+    HelloWorld: HelloWorldVue,
+  },
+});
+</script>
