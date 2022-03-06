ee4085df28bd822c17bc8d9858338559743a682d
Rahul Kadyan <hi@znck.me>
2022-03-06 00:30:09 -0800

    generateUUID fn

--- packages/shared/src/string.ts
+++ packages/shared/src/string.ts
@@ -50,3 +50,10 @@ export function isPascalCase(str: string): boolean {
 export function isCamelCase(str: string): boolean {
   return /^[a-z][A-Za-z0-9]*$/.test(str)
 }
+
+export function generateUUID(): string {
+  return new Array(4)
+    .fill(0)
+    .map(() => Math.floor(Math.random() * Number.MAX_SAFE_INTEGER).toString(16))
+    .join('-')
+}
